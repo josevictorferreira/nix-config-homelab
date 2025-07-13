b2ff907f3d0f3e344725de66d5f65510d6b5a0e7
Jose Victor Ferreira <root@josevictor.me>
2025-07-13 00:29:09 -0300

    Added ssh public key and readme.md init file

--- hosts/raspberry-pi4.nix
+++ hosts/raspberry-pi4.nix
@@ -32,6 +32,7 @@
     extraGroups = [ "wheel" ];
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINLyucWDDYGQ4sg4biPGOTxXo9xoYCY816t4koLi07+x root@josevictor.me"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPAXdWHFx9UwUOXlapiVD0mzM0KL9VsMlblMAc46D9PV josevictor@josevictor-nixos"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOVNsxVT6rzeyqZVlJVdQgKEzK2z0fOFNRZMAvQvBxbX josevictorferreira@macos-macbook"
     ];
