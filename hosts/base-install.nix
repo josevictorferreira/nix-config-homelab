f52237304a974a62738b1d47febc4702c69cc51f
Jose Victor Ferreira <root@josevictor.me>
2025-07-25 15:57:55 -0300

    updated nix

--- hosts/base-install.nix
+++ hosts/base-install.nix
@@ -16,6 +16,17 @@
   networking.hostName = "base";
   networking.useDHCP = true;
 
+  users.users.josevictor = {
+    isNormalUser = true;
+    home = "/home/josevictor";
+    extraGroups = [ "wheel" ];
+    shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPAXdWHFx9UwUOXlapiVD0mzM0KL9VsMlblMAc46D9PV josevictor@josevictor-nixos"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOVNsxVT6rzeyqZVlJVdQgKEzK2z0fOFNRZMAvQvBxbX josevictorferreira@macos-macbook"
+    ];
+  };
+
   security.sudo = {
     enable = true;
     wheelNeedsPassword = false;
